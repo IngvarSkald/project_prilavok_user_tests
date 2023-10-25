49df748795557f7ca9a3da9f70048b4f93223e91
Igor <igoryan.bespalov@yandex.ru>
2023-10-25 21:16:58 +0300

    add data

---
+++ venv/data.py
@@ -0,0 +1,15 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Анатолий",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
+
+kit_body = {
+    "name": "Мой набор"
+}
+
+auth_token = {"Authorization": "Bearer {'5206d541-cfcc-466d-9bfa-9c7684ee00aa'}"}
